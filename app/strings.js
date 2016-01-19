dd2ca7217da4d9e83722bb8bad7d4456d7b9dfbf
Doug Shamoo <doug.shamoo@gmail.com>
2016-01-18 21:50:31 -0800

    Complete strings

--- app/strings.js
+++ app/strings.js
@@ -2,12 +2,56 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+    var count = 1;
+    var letters = str.split('').reduce(function(word, ch) {
+      if (ch !== word[word.length - 1]) {
+        // append letter, start new count
+        word += ch;
+        count = 1;
+      } else if (count < amount) {
+        // apend letter, increment count
+        word += ch;
+        count++;
+      } // otherwise ignore
+      return word;
+    });
+    return letters;
   },
+  // TODO: super naive, find the more elegant solution that must exist
   wordWrap: function(str, cols) {
+    var count = 0;
+    var words = str.split(' ');
 
+    return words.reduce(function(formatted, word, index) {
+      var len = word.length;
+
+      if (count === 0) {
+        // at beginning of newline
+        formatted += word;
+        count += len;
+      } else if (count > 0 && count < cols) {
+        // if word fits on line
+        if (count + len + 1 <= cols) {
+          formatted += ' ' + word;
+          count += len + 1;
+        } else { // if word doesn't fit on line
+          formatted += '\n' + word;
+          count += len;          
+        }
+      }
+
+      if (count >= cols && index !== words.length - 1) {
+        // go to newline and reset count
+        formatted += '\n';
+        count = 0;
+      }
+
+      return formatted;
+    }, '');
+
+    // remove last newline and return
   },
   reverseString: function(str) {
-
+    return str.split('').reverse().join('');
   }
 };
