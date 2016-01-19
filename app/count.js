82c73cbc7f761d45d5468d065a466c407018a6a7
Doug Shamoo <doug.shamoo@gmail.com>
2016-01-18 20:15:40 -0800

    Complete count

--- app/count.js
+++ app/count.js
@@ -2,6 +2,17 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
-
+    // should count from start number to end number, one per 1/10 second
+    var num = start;
+    console.log(num);
+    var timerId = setInterval(function() {
+      if (++num > end) clearInterval(timerId);
+      else console.log(num);
+    }, 100);
+    return {
+      cancel: function() {
+        clearInterval(timerId);
+      },
+    };
   }
 };
