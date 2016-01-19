98ba31f8efa7c5a63bfc5d4a6e8e1730618b9a64
Doug Shamoo <doug.shamoo@gmail.com>
2016-01-18 21:07:40 -0800

    Complete objects

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,19 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
+    var props = Object.getOwnPropertyNames(obj);
 
+    return props.reduce(function(result, prop) {
+      result.push(prop + ': ' + obj[prop]);
+      return result;
+    }, []);
   }
 };
