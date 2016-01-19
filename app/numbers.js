c2c698a1b10a84abeee1aa2568c3afb04e4407bc
Doug Shamoo <doug.shamoo@gmail.com>
2016-01-18 20:40:19 -0800

    Complete numbers

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,26 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
+    var binString = num.toString(2);
 
+    return parseInt(binString[binString.length - bit]);
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
+    var binString = num.toString(2);
 
+    while (binString.length < 8) {
+      binString = '0' + binString;
+    }
+
+    return binString;
   },
 
   multiply: function(a, b) {
-
+    return a * b;
   }
 };
